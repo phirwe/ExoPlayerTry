2be3d6127b90cd234fced2ac6c11f25239f553c4
Poorwa Hirve <poorwa.hirve@workday.com>
2018-06-18 11:40:14 -0600

    added support for many video / audio types

--- app/src/main/java/com/example/poorwahirve/exoplayer/MainActivity.kt
+++ app/src/main/java/com/example/poorwahirve/exoplayer/MainActivity.kt
@@ -31,9 +31,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.exo_playback_control_view.*
 import java.text.ParsePosition
 import com.google.android.exoplayer2.ui.PlaybackControlView
-
-
-
+import java.lang.Math.random
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), Player.EventListener {
@@ -42,6 +41,9 @@ class MainActivity : AppCompatActivity(), Player.EventListener {
     private var playbackPosition = 0L
     private val dashUrl = "http://rdmedia.bbc.co.uk/dash/ondemand/bbb/2/client_manifest-separate_init.mpd"
     private val mp4Url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+
+    private val urlArray : Array<String> = arrayOf(dashUrl, mp4Url)
+
     private lateinit var fullScreenDialog : Dialog
     private var fullScreen = false
 //    private val bandwidthMeter = DefaultBandwidthMeter()
@@ -105,16 +107,25 @@ class MainActivity : AppCompatActivity(), Player.EventListener {
     }
 
     // helper method to create the media source
-    private fun buildMediaSource(uri: Uri): MediaSource {
+    private fun buildMediaSource(uri: String): MediaSource {
+        val uriParsed = Uri.parse(uri)
+
+        if ( (uri as CharSequence).contains(".mpd", false) ) {
+            val dataSourceFactory = DefaultHttpDataSourceFactory("ua", bandwidthMeter)
+            val dashChunkSourceFactory = DefaultDashChunkSource.Factory(dataSourceFactory)
+            return DashMediaSource(uriParsed, dataSourceFactory, dashChunkSourceFactory, null, null)
+        }
+
         val dataSourceFactory = DefaultDataSourceFactory(this,"ua", bandwidthMeter)
         val dashChunkSourceFactory = DefaultDashChunkSource.Factory(dataSourceFactory)
         val extractorsFactory = DefaultExtractorsFactory()
-        return ExtractorMediaSource(uri, dataSourceFactory, extractorsFactory, null, null)
+        return ExtractorMediaSource(uriParsed, dataSourceFactory, extractorsFactory, null, null)
     }
 
     // prepare the exoplayer
     private fun prepareExoPlayer() {
-        val uri = Uri.parse(mp4Url)
+        val uri = urlArray[(0..urlArray.size).random()]
+        Log.e("URL", uri)
         val videoSource = buildMediaSource(uri)
         simpleExoPlayer.prepare(videoSource)
     }
@@ -210,4 +221,87 @@ class MainActivity : AppCompatActivity(), Player.EventListener {
     override fun onTimelineChanged(timeline: Timeline?, manifest: Any?) {
         
     }
+
+    fun ClosedRange<Int>.random() =
+            Random().nextInt(endInclusive - start) +  start
+
 }
+
+
+/*
+
+package com.example.poorwahirve.exoplayer
+
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.TrackGroupArray
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.ParsePosition
+
+class MainActivity : AppCompatActivity(), Player.EventListener {
+
+    private lateinit var simpleExoPlayer: SimpleExoPlayer
+    private var playbackPosition = 0L
+    private val dashUrl = "http://rdmedia.bbc.co.uk/dash/ondemand/bbb/2/client_manifest-separate_init.mpd"
+    private val mp4Url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+
+    private val bandwidthMeter by lazy {
+        DefaultBandwidthMeter()
+    }
+    private val adaptiveTrackSelectionFactory by lazy {
+        AdaptiveTrackSelection.Factory(bandwidthMeter)
+    }
+
+    // helper method to create the media source
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val dataSourceFactory = DefaultHttpDataSourceFactory("ua", bandwidthMeter)
+        val dashChunkSourceFactory = DefaultDashChunkSource.Factory(dataSourceFactory)
+        return DashMediaSource(uri, dataSourceFactory, dashChunkSourceFactory, null, null)
+    }
+
+    // prepare the exoplayer
+    private fun prepareExoPlayer() {
+        val uri = Uri.parse(mp4Url)
+        val videoSource = buildMediaSource(uri)
+        simpleExoPlayer.prepare(videoSource)
+    }
+
+    // init exoplayer
+    private fun initializeExoPlayer() {
+        simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(
+                DefaultRenderersFactory(this),
+                DefaultTrackSelector(adaptiveTrackSelectionFactory),
+                DefaultLoadControl()
+        )
+
+        prepareExoPlayer()
+        simpleExoPlayerView.player = simpleExoPlayer
+        simpleExoPlayer.seekTo(playbackPosition)
+        simpleExoPlayer.playWhenReady = true
+        simpleExoPlayer.addListener(this)
+    }
+
+
+
+
+}
+
+
+
+
+ */
